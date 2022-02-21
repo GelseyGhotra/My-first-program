6d76ddb100e1654aabb92b8096f710aa1f0785fa
GelseyGhotra <100119133+GelseyGhotra@users.noreply.github.com>
2022-02-21 18:49:47 +1030

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,6 @@
+
+console.log("hello");
+console.log("welcome to my first program");
+console.log("my name is Gelsey Ghotra");
+console.log("I live in South Australia");
+console.log("I am 13 years old");
